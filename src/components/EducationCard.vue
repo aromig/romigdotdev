38a16235c422a5d9a2e1aad3a8c7a04ccdb9d7d1
Adam Romig <adam.romig@gmail.com>
2019-03-22 12:49:08 -0400

    scoping education/workexperience component styles

--- src/components/EducationCard.vue
+++ src/components/EducationCard.vue
@@ -61,7 +61,7 @@ export default {
 };
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
 .education-card {
   margin-bottom: 25px;
 }
--- src/components/WorkExperienceCard.vue
+++ src/components/WorkExperienceCard.vue
@@ -71,7 +71,7 @@ export default {
 };
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
 a {
   text-decoration: none;
 }
